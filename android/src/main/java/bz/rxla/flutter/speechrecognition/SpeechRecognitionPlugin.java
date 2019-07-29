f50ce21fac03f2d32d82091d926df3449ada4830
Scott Choi <yhlivefor@gmail.com>
2019-07-29 14:17:45 +0900

    Fix default response to en_US

--- android/src/main/java/bz/rxla/flutter/speechrecognition/SpeechRecognitionPlugin.java
+++ android/src/main/java/bz/rxla/flutter/speechrecognition/SpeechRecognitionPlugin.java
@@ -50,6 +50,7 @@ public class SpeechRecognitionPlugin implements MethodCallHandler, RecognitionLi
                 RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
         recognizerIntent.putExtra(RecognizerIntent.EXTRA_PARTIAL_RESULTS, true);
         recognizerIntent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, 3);
+        recognizerIntent.putExtra(RecognizerIntent.EXTRA_ONLY_RETURN_LANGUAGE_PREFERENCE, 'en_US');
     }
 
     @Override
